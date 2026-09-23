ef59fd7eb32d1b7b185e729ae92f1efac7a54255
Cesar Garcia <2630328@upv.edu.mx>
2026-09-22 22:55:36 -0600

    clase de nombramiento de variables

---
+++ src/under_variables.py
@@ -0,0 +1,29 @@
+#lets try to use variables 
+message =  "this is my firts program lol XD, i am very happy"
+another_message = "Really, so so so so sad :("
+
+print(message)
+print(another_message)
+
+print(message, another_message, "no joke")
+
+message = "i eat soap"
+print(message)
+
+"""
+Reglas generales para nombrar variables en python
+
+Los nombres de variables solo se nombran en ingles
+
+-letras, numeros y guión baja.
+-deben comenzar con letra o guión bajo, pero no numeros.
+-no utilizar palabras reservadas de python o el nombre del
+ archivo con nombre de variables.
+ -Los nombres deben ser cortos pero descriptivos.
+ -letras minisculas for NOW
+
+ Ejemplo:
+ correcto: esotilin_1
+ incorrecto: 1esotilin_1
+ 
+"""
