367610b5794faf88b83d869b1aec6548d1e0f5e6
Cesar Garcia <2630328@upv.edu.mx>
2026-09-23 19:53:58 -0600

    Clase de sintaxis y numberos

---
+++ src/understanding_numbers.py
@@ -0,0 +1,16 @@
+#Numeros
+#Enteros - Integers
+
+"""
+Los numeros enteros los podemos sumar (+), restar (-),
+ multiplicar (*) y dividir (/)
+"""
+
+print(5+1)
+print(3-2)
+print(2*5)
+print(1/2)
+
+number_1 = 5
+number_2 = 10
+print(number_1+number_2)
